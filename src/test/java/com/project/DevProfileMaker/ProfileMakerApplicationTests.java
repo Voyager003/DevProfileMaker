9493d78a3162db58d726c6e024229f27a0b605ea
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-09-30 00:17:28 +0900

    Initial commit

---
+++ src/test/java/com/project/DevProfileMaker/ProfileMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.DevProfileMaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
